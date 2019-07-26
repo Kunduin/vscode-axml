9518a312ff84f3c8d9f83f66c5df2e886f480ca5
Kunduin <15852158126@163.com>
2019-07-26 15:29:19 +0800

    feat(client): add few config

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,27 +3,29 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as path from "path";
+import { languages, ExtensionContext, IndentAction } from "vscode";
 
 import {
   LanguageClient,
   LanguageClientOptions,
   ServerOptions,
   TransportKind
-} from 'vscode-languageclient';
+} from "vscode-languageclient";
+
+const EMPTY_ELEMENTS = ["image", "input"];
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
   // The server is implemented in node
   let serverModule = context.asAbsolutePath(
-    path.join('server', 'out', 'server.js')
+    path.join("server", "out", "server.js")
   );
   console.log(serverModule);
   // The debug options for the server
   // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
@@ -39,19 +41,48 @@ export function activate(context: ExtensionContext) {
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
     // Register the server for plain text documents
-    documentSelector: [{ scheme: 'file', language: 'axml' }]
+    documentSelector: [{ scheme: "file", language: "axml" }]
   };
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'AXMLServer',
-    'AXML Server',
+    "AXMLServer",
+    "AXML Server",
     serverOptions,
     clientOptions
   );
 
   // Start the client. This will also launch the server
   client.start();
+
+  languages.setLanguageConfiguration("axml", {
+    indentationRules: {
+      increaseIndentPattern: /<(?!\?|(?:image|input)\b|[^>]*\/>)([-_\.A-Za-z0-9]+)(?=\s|>)\b[^>]*>(?!.*<\/\1>)|<!--(?!.*-->)|\{[^}"']*$/,
+      decreaseIndentPattern: /^\s*(<\/[-_\.A-Za-z0-9]+\b[^>]*>|-->|\})/
+    },
+    wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
+    onEnterRules: [
+      {
+        beforeText: new RegExp(
+          `<(?!(?:${EMPTY_ELEMENTS.join(
+            "|"
+          )}))([_:\\w][_:\\w-.\\d]*)([^/>]*(?!/)>)[^<]*$`,
+          "i"
+        ),
+        afterText: /^<\/([_:\w][_:\w-.\d]*)\s*>/i,
+        action: { indentAction: IndentAction.IndentOutdent }
+      },
+      {
+        beforeText: new RegExp(
+          `<(?!(?:${EMPTY_ELEMENTS.join(
+            "|"
+          )}))(\\w[\\w\\d]*)([^/>]*(?!/)>)[^<]*$`,
+          "i"
+        ),
+        action: { indentAction: IndentAction.Indent }
+      }
+    ]
+  });
 }
 
 export function deactivate(): Thenable<void> | undefined {
